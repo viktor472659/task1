dd91ee43b55c0e06f8ab3e6d585738b92fb2fb84
Віктор Семернін <viktor.semernin@uzhnu.edu.ua>
2020-11-27 18:07:41 +0200

    Initial Commit

---
+++ task1/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  task1
+//
+//  Created by Viktor on 27.11.2020.
+//  Copyright © 2020 Viktor. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ task1/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  task1
+//
+//  Created by Viktor on 27.11.2020.
+//  Copyright © 2020 Viktor. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
